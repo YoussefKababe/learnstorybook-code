c13b83ded33f85f84b66350175286ad7f5ce57ae
YoussefKababe <youssef.kbe@gmail.com>
2019-10-06 20:00:39 +0100

    Update tests

--- src/storybook-image.test.js
+++ src/storybook-image.test.js
@@ -1,10 +1,13 @@
 import initStoryshots from "@storybook/addon-storyshots";
 import { imageSnapshot } from "@storybook/addon-storyshots-puppeteer";
-import devices from "puppeteer/DeviceDescriptors";
 
-const tablet = devices["iPad landscape"];
-const customizePage = page => page.emulate(tablet);
-const beforeScreenshot = page => page.emulate(tablet);
+const beforeScreenshot = page =>
+  page.setViewport({
+    width: 800,
+    height: 800,
+    deviceScaleFactor: 2
+  });
+
 const getMatchOptions = () => ({
   failureThreshold: 0.2,
   failureThresholdType: "percent"
@@ -14,7 +17,6 @@ initStoryshots({
   suite: "Image storyshots",
   test: imageSnapshot({
     storybookUrl: "http://localhost:9009",
-    customizePage,
     beforeScreenshot,
     getMatchOptions
   })
