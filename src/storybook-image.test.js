87a6dd1339f5d49915018442707866c20b28648d
YoussefKababe <youssef.kbe@gmail.com>
2019-10-06 09:36:08 +0100

    Modify puppeteer viewport

--- src/storybook-image.test.js
+++ src/storybook-image.test.js
@@ -1,9 +1,21 @@
 import initStoryshots from "@storybook/addon-storyshots";
 import { imageSnapshot } from "@storybook/addon-storyshots-puppeteer";
+import devices from "puppeteer/DeviceDescriptors";
+
+const tablet = devices["iPad landscape"];
+const customizePage = page => page.emulate(tablet);
+const beforeScreenshot = page => page.emulate(tablet);
+const getMatchOptions = () => ({
+  failureThreshold: 0.2,
+  failureThresholdType: "percent"
+});
 
 initStoryshots({
   suite: "Image storyshots",
   test: imageSnapshot({
-    storybookUrl: "http://localhost:9009"
+    storybookUrl: "http://localhost:9009",
+    customizePage,
+    beforeScreenshot,
+    getMatchOptions
   })
 });
